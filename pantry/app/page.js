ad9eda14bd7f95a63801db371ace015ffdf21cf8
Adewole Adeoshun <woleadeoshun18@gmail.com>
2024-09-14 16:17:42 -0400

    Pantry tracker

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,7 +1,7 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 const item =[
-  'tomato','onion','potato','garlic','ginger','carrot'
+  'tomato','onion','potato','garlic','ginger','carrot','lettuce','kale','cucumber '
 ]
 
 
@@ -12,25 +12,47 @@ export default function Home() {
     height ="100vh"
     display={'flex'}
     justifyContent={'center'}
-    alignItems={'center'}> 
-      <Stack width ="800px" height= "600px" spacing ={2}>
+    flexDirection={'column'}
+    alignItems={'center'}
+    > 
+     <Box border={'1px solid #333'}> 
+    <Box width ="800px" height ="100px" bgcolor={'#FFCCCB'} >
+    <Typography variant ={'h2'} color ={'#333'} textAlign = {'center'}>
+      Pantry Items
+      </Typography>
+    </Box>
+      <Stack width ="800px" height= "300px" spacing ={2} overflow={'auto'}>
 
       {item.map((i) => (
         <Box
           key={i}
           width = "100%" 
-          height ="100px"
+          height ="300px"
           display={'flex'}
           justifyContent={'center'}
           alignItems={'center'}
           bgcolor={'#f0f0f0'}
           >
-            {i}
+            
+            <Typography 
+            variant={'h3'}
+            color ={'#333'}
+            textAlign={'center'}
+            
+            
+            >
+              {
+                //Capitalize the first letter item
+              i.charAt(0).toUpperCase() + i.slice(1)
+              }
+            </Typography>
+        
+          
           </Box>
 
       ))}
      </Stack>
-
+    </Box>
 
     </Box>
 
