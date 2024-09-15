7804e5376f00acff269b82baf358eacaf2bfdf13
Adewole Adeoshun <woleadeoshun18@gmail.com>
2024-09-14 22:07:23 -0400

    Pantry tracker

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,14 +1,32 @@
 "use client";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography,Button, Modal,TextField} from "@mui/material";
 import {firestore} from "@/firebase";
 import {collection,query,getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { getFirestore } from "firebase/firestore";
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'White',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
-// Your Firebase configuration
+//  Firebase configuration
 export default function Home() {
   const [pantry, setPantry]= useState ([])
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const[itemName, setItemName] = useState('')
+
 
   useEffect(() => {
     const updatePantry = async () => {
@@ -32,12 +50,34 @@ export default function Home() {
     justifyContent={'center'}
     flexDirection={'column'}
     alignItems={'center'}
+    gap={2}
     > 
+
+
+    <Modal 
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width="100%" direction = {'row'} spacing ={2}>
+      <TextField id="outlined-basic" label="Item" variant="outlined" />
+      <Button variant="contained">Search</Button>  
+      </Stack>
+        </Box>
+        </Modal>
+     <Button variant="contained" onClick={handleOpen}>
+        Add</Button>
      <Box border={'1px solid #333'}> 
     <Box width ="800px" height ="100px" bgcolor={'#FFCCCB'} >
     <Typography variant ={'h2'} color ={'#333'} textAlign = {'center'}>
       Pantry Items
       </Typography>
+      
     </Box>
       <Stack width ="800px" height= "300px" spacing ={2} overflow={'auto'}>
 
